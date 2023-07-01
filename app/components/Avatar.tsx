56571b6e9217d0ffb23ccba17fcd45b9e96e9d44
PtDat <phungdat020501@gmail.com>
2023-07-01 10:12:18 +0700

    feat: social-login

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,12 +1,16 @@
 "use client";
 import Image from "next/image";
 
-const Avatar = () => {
+interface AvatarProps {
+  src?: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <Image
       className="rounded-full"
       alt="avatar"
-      src="/images/placeholder.jpg"
+      src={src || "/images/placeholder.jpg"}
       width="30"
       height="30"
     ></Image>
