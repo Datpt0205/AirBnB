570c53de12aee8f1e40fe18e3d93db698de973a2
PtDat <phungdat020501@gmail.com>
2023-07-14 14:28:08 +0700

    hotfix: deploy-error

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-interface HomeProps{
-  searchParams: IListingsParams
+interface HomeProps {
+  searchParams: IListingsParams;
 }
 
-const Home = async ({searchParams}: HomeProps) => {
+const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
@@ -25,19 +25,17 @@ const Home = async ({searchParams}: HomeProps) => {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl-grid-cols-6 gap-8">
-          {listings.map((listing) => {
-            return (
-              <ListingCard
-                currentUser={currentUser}
-                key={listing.id}
-                data={listing}
-              />
-            );
-          })}
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
   );
-}
+};
 
-export default Home
+export default Home;
